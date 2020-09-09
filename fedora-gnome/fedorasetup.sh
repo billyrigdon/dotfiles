1d9510fbeff99e9df17b65323d9022230f081b53
billyrigdon <billy@rigdonsolutions.com>
2020-09-09 09:08:50 -0500

    Improved structure, created light/dark mode fedora scripts, improved fedorasetup.sh

---
+++ fedora-gnome/fedorasetup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+sudo dnf install \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  
+sudo dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm 
+ 
+sudo dnf install gnome-tweak-tool gnome-shell-extension-user-theme chromium icedtea-web java-latest-openjdk virt-manager snapd arc-theme numix-icon-theme-circlevim gnome-shell-extension-no-topleft-hot-corner
+
+sudo systemctl enable snapd
+sudo systemctl start snapd
+
+sudo ln -s /var/lib/snapd/snap /snap
+sudo snap install code --classic
+sudo snap install powershell --classic
+
+ssh-add ~/.ssh/id_rsa
+
+mkdir ~/.scripts
+sudo cp -r ./lightmode ~/.scripts/lightmode
+sudo cp -r ./darkmode ~/scripts/darkmode
+echo "echo export PATH="/home/billy/.scripts:$PATH"" >> ~/.bashrc
+
+#gsettings set org.gnome.desktop.interface gtk-theme Arc
+#gsettings set org.gnome.desktop.interface gtk-theme Arc-Dark
+
+rm -r ~/Public
+rm -r ~/Templates
+mkdir ~/Git
+mv ./Wallpapers ~/Pictures/
